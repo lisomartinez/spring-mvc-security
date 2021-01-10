e41a6666d2aa90c9dffcac2c4990f0ecb4113455
Lisandro Martinez <lisandromartinez@gmail.com>
2021-01-10 12:13:11 -0300

    initial commit

---
+++ src/test/java/ar/coders/springmvcsecurity/SpringMvcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.coders.springmvcsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
